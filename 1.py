1265bb30f9ac3675d5f0e88b95012469a658132e
Robert Filipkowski <rob@dateit.io>
2022-12-01 22:59:10 -0500

    done

---
+++ 1.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from operator import methodcaller
+from itertools import groupby
+
+directory = os.getcwd()
+pred = lambda x: x == " "
+with open('elfs.txt') as fp:
+    lines = fp.read().split('\n')
+    chunks = (list(g) for k,g in groupby(lines, key=lambda x: x != '') if k)
+    reducedList = []
+    for chunk in chunks:
+        to_ints = [eval(i) for i in chunk]
+        reduced = sum(to_ints)
+        reducedList.append(reduced)
+    maxV = max(reducedList)
+    print(maxV)
+    
